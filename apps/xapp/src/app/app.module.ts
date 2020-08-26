4aca94ed4576e83a30c482158e0df2fb203249db
Keiji <keiji@kgpc-wsl2>
2020-08-25 23:36:21 -0500

    OP-0 added example lib with hello component

--- apps/xapp/src/app/app.module.ts
+++ apps/xapp/src/app/app.module.ts
@@ -2,10 +2,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ExampleModule } from '@client/example';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ExampleModule],
   providers: [],
   bootstrap: [AppComponent],
 })
